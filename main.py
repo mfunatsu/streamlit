09cad89087db2efc5f807e9d4277edde7a26aa1e
masashi.funatsu <m_funatsu@masashifunatsunoMacBook-Pro.local>
2022-10-22 15:21:42 +0900

    2nd commit

--- main.py
+++ main.py
@@ -33,59 +33,59 @@ if button:
 expander=st.expander('問合せ')
 expander.write('問合せ内容を書く')
 
-# st.write('Display Image')
+st.write('Display Image')
 
-# option = st.selectbox(
-#     '好きなもの',
-#     list(range(1, 10))
-# )
+option = st.selectbox(
+    '好きなもの',
+    list(range(1, 10))
+)
 
-# 'あなたの好きな数字は',option,'です。'
+'あなたの好きな数字は',option,'です。'
 
-# if st.checkbox('show image'):
-#     img = Image.open('今月も手取りがこれだけ.jpeg')
-#     st.image(img, caption='okane', use_column_width=True)
+if st.checkbox('show image'):
+    img = Image.open('sample.jpeg')
+    st.image(img, caption='okane', use_column_width=True)
 
 
-# df =pd.DataFrame({
-#     '1列目':[1,2,3,4],
-#     '2列目':[10,20,30,40]
-# })
-# st.write(df)
-# st.dataframe(df.style.highlight_max(axis=0),width=180,height=180)
-# st.table(df.style.highlight_max(axis=0))
+df =pd.DataFrame({
+    '1列目':[1,2,3,4],
+    '2列目':[10,20,30,40]
+})
+st.write(df)
+st.dataframe(df.style.highlight_max(axis=0),width=180,height=180)
+st.table(df.style.highlight_max(axis=0))
 
-# """
+"""
 
-# # あああ
+# あああ
 
-# ## いいい
-# ``` python
-# st.write("明細")
+## いいい
+``` python
+st.write("明細")
 
-# df =pd.DataFrame({
-#     '1列目':[1,2,3,4],
-#     '2列目':[10,20,30,40]
-# })
-# st.write(df)
-# st.dataframe(df.style.highlight_max(axis=0),width=180,height=180)
-# st.table(df.style.highlight_max(axis=0))
-# ```
-# ### ううう
+df =pd.DataFrame({
+    '1列目':[1,2,3,4],
+    '2列目':[10,20,30,40]
+})
+st.write(df)
+st.dataframe(df.style.highlight_max(axis=0),width=180,height=180)
+st.table(df.style.highlight_max(axis=0))
+```
+### ううう
 
-# """
+"""
 
-# df2 =pd.DataFrame(
-#     np.random.rand(20,3),
-#     columns=['a','b','c']
-# )
-# st.line_chart(df2)
-# st.area_chart(df2)
-# st.bar_chart(df2)
+df2 =pd.DataFrame(
+    np.random.rand(20,3),
+    columns=['a','b','c']
+)
+st.line_chart(df2)
+st.area_chart(df2)
+st.bar_chart(df2)
 
-# df3 =pd.DataFrame(
-#     np.random.rand(100,2)/[50,50]+[35.69,139.70],
-#     columns=['lat','lon']
-# )
+df3 =pd.DataFrame(
+    np.random.rand(100,2)/[50,50]+[35.69,139.70],
+    columns=['lat','lon']
+)
 
-# st.map(df3)
+st.map(df3)
